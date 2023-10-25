91d2843c5960494804dcd498075bbdd8241cbf87
KubotaAtsuro <achaemon10@icloud.com>
2023-10-25 10:53:57 +0900

    地雷による敵へのダメージ処理を追加

--- Assets/Scripts/Object/Mine.cs
+++ Assets/Scripts/Object/Mine.cs
@@ -3,46 +3,51 @@ using UnityEngine;
 
 public class Mine : MonoBehaviour
 {
+    //敵1
+    [SerializeField] GameObject Enemy1;
     //地雷の攻撃力
     private int mine_Damage;
     //死ぬまでのカウント
-    float damage_countDown;
+    private float damage_countDown;
     //死ぬ時間
-    float death_Time;
+    private float death_Time;
     void Start()
     {
         mine_Damage = 100; //仮ダメージ
         damage_countDown = 0.0f;
-        death_Time = 10.0f;
-    }
-
-    void Update()
-    {
-
+        death_Time = 100.0f;
     }
 
     //ダメージ発生カウント
-    public void Damage()
+    private void Damage_Count()
     {
-        
         damage_countDown += Time.deltaTime;
 
-        //数秒経ったら消滅
-        if (damage_countDown <= death_Time)
+        //10秒経ったら消滅
+        if (damage_countDown >= death_Time)
         {
             Destroy(gameObject);
         }
     }
 
+    //地雷に触れた時
     void OnCollisionEnter(Collision collision)
     {
-        //if (collision.gameObject.name == "Player")
-        //{
-        //    var message = new DamageMessage();
-        //    message.amount = mine_Damage; //ダメージ
-        //    message.damager = gameObject; //攻撃側
-        //    Debug.Log("amout：" + message.amount);
-        //    Debug.Log("damager:" + message.damager);
-        //}
+        IDamageable damageable = collision.gameObject.GetComponent<IDamageable>();
+
+        //ダメージ処理が発生したとき
+        if (damageable != null)
+        {
+            //敵1が接触したとき
+            if (collision.gameObject == Enemy1)
+            {
+
+                damageable.Damage(mine_Damage);
+
+                Damage_Count();
+
+                Debug.Log(collision.gameObject + "に" + mine_Damage + "ダメージを与える");
+            }
+        }
     }
 }
--- Assets/Scripts/Utility/IDamageable.cs
+++ Assets/Scripts/Utility/IDamageable.cs
@@ -1,4 +1,7 @@
 public interface IDamageable
 {
     bool ApplyDamage(DamageMessage damageMessage);
+
+    //ƒ_ƒ[ƒWˆ—
+    public void Damage(int damage);
 }
