af996e1dfdd15ba35a18fe93789a2599f39a0eaa
loaqloaqloaq <63897284+loaqloaqloaq@users.noreply.github.com>
2023-12-14 10:41:29 +0900

    ミニマップクリックでテレポート

--- Assets/Main/Scripts/Map/MapController.cs
+++ Assets/Main/Scripts/Map/MapController.cs
@@ -1,12 +1,17 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class MapController : MonoBehaviour
+public class MapController : MonoBehaviour, IPointerClickHandler
 {
     // Start is called before the first frame update
     [SerializeField] public bool enable;
     [SerializeField] GameObject[] children;
     Camera cam;
     [SerializeField] float maxZoom, minZoom;
+    [SerializeField] LayerMask whatIsTarget;
+
+    RawImage miniMapImage;
 
     Vector2 prevPos, curPos;
 
@@ -16,6 +21,8 @@ public class MapController : MonoBehaviour
     float[] maxZ, maxX,minZ,minX; 
     float maxZdif, maxXdif, minZdif, minXdif;
     public float zoomPresent;
+
+    float mouseHoldTime;
 
     Transform player;
     void Start()
@@ -27,9 +34,8 @@ public class MapController : MonoBehaviour
         }
         cam = children[0].GetComponent<Camera>();
 
-        playerInput = GameObject.FindWithTag("Player")?.GetComponent<PlayerInput>() ?? null;
-
         player = GameObject.FindWithTag("Player").transform;
+        playerInput = player?.GetComponent<PlayerInput>() ?? null;        
 
         prevPos =Vector2.zero; curPos=Vector2.zero;
 
@@ -39,6 +45,10 @@ public class MapController : MonoBehaviour
         minXdif = minX[0] - minX[1];
 
         zoomPresent = 1 - ((cam.orthographicSize - minZoom) / (maxZoom - minZoom));
+
+        mouseHoldTime = 0;
+
+        miniMapImage = transform.GetChild(1).GetChild(0).GetChild(0).GetComponent<RawImage>();
 
     }
 
@@ -50,21 +60,7 @@ public class MapController : MonoBehaviour
 
         //マップの表示や非表示
         if (Input.GetKeyDown(KeyCode.M) && !isPause) {
-            enable = !enable;
-            foreach (GameObject child in children) { 
-                child.SetActive(enable);
-            }
-            if (playerInput) playerInput.enabled = !enable;
-            UIManager.Instance?.SetMouseVisible(enable);
-            //mapCanvas.enabled = enable;
-            if (player) {
-                cam.transform.position = new Vector3(
-                    player.position.x,
-                    cam.transform.position.y,
-                    player.position.z
-                    );
-                cam.transform.position = CheckMargin(cam.transform.position);
-            }
+            SetVisible(!enable);
         }
         //マップのコントロール
         if (enable)
@@ -83,19 +79,28 @@ public class MapController : MonoBehaviour
             {
                 curPos = Input.mousePosition;
                 prevPos = Input.mousePosition;
+                mouseHoldTime = 0;
             }
-            if (Input.GetMouseButton(0)) {
+            if (Input.GetMouseButton(0))
+            {
+                mouseHoldTime += Time.deltaTime;
                 curPos = Input.mousePosition;
-                if (Vector2.Distance(prevPos, curPos) != 0) { 
-                    Vector2 dis = curPos- prevPos;
+                if (Vector2.Distance(prevPos, curPos) != 0)
+                {
+                    Vector2 dis = curPos - prevPos;
                     Vector3 pos = cam.transform.position;
-                    pos.x += dis.y *(cam.orthographicSize/200) ;
+                    pos.x += dis.y * (cam.orthographicSize / 200);
                     pos.z -= dis.x * (cam.orthographicSize / 200);
                     cam.transform.position = CheckMargin(pos);
                     prevPos = curPos;
                 }
             }
-        }        
+            if (Input.GetMouseButtonUp(0)&& mouseHoldTime < 0.2f)
+            {
+                Debug.Log("Clicked "+ Input.mousePosition.ToString());
+                
+            }
+        }
     }
 
     Vector3 CheckMargin(Vector3 pos) {
@@ -103,6 +108,7 @@ public class MapController : MonoBehaviour
         float minX = this.minX[1] + minXdif * zoomPresent;
         float maxX = this.maxX[1] + maxXdif * zoomPresent;
         float minZ = this.minZ[1] + minZdif * zoomPresent;
+
         float maxZ = this.maxZ[1] + maxZdif * zoomPresent;
 
 
@@ -111,4 +117,67 @@ public class MapController : MonoBehaviour
         
         return pos;
     }
-}
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (mouseHoldTime >= 0.2f) return;
+        Vector2 curosr = new Vector2(0, 0);
+
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(miniMapImage.rectTransform,
+            eventData.pressPosition, eventData.pressEventCamera, out curosr))
+        {
+
+            Texture texture = miniMapImage.texture;
+            Rect rect = miniMapImage.rectTransform.rect;
+
+            float coordX = Mathf.Clamp(0, (((curosr.x - rect.x) * texture.width) / rect.width), texture.width);
+            float coordY = Mathf.Clamp(0, (((curosr.y - rect.y) * texture.height) / rect.height), texture.height);
+
+            float calX = coordX / texture.width;
+            float calY = coordY / texture.height;
+
+
+            curosr = new Vector2(calX, calY);
+           
+            CastRayToWorld(curosr);
+        }
+
+    }
+
+    private void CastRayToWorld(Vector2 vec)
+    {
+        Ray MapRay = cam.ScreenPointToRay(new Vector2(vec.x * cam.pixelWidth,
+            vec.y * cam.pixelHeight));
+
+        RaycastHit hit;        
+
+        if (Physics.Raycast(MapRay, out hit, Mathf.Infinity, whatIsTarget))
+        {
+            //Debug.Log("miniMapHit: " + hit.point.ToString());
+            player.GetComponent<PlayerTeleport>().TeleportTo(hit.point);
+            SetVisible(false);
+        }
+
+    }
+
+    private void SetVisible(bool en) {
+        enable = en;
+        foreach (GameObject child in children)
+        {
+            child.SetActive(enable);
+        }
+        if (playerInput) playerInput.enabled = !enable;
+        UIManager.Instance?.SetMouseVisible(enable);
+        //mapCanvas.enabled = enable;
+        if (player && enable)
+        {
+            cam.transform.position = new Vector3(
+                player.position.x,
+                cam.transform.position.y,
+                player.position.z
+                );
+            cam.transform.position = CheckMargin(cam.transform.position);
+        }
+    }
+
+ }
--- Assets/Main/Scripts/Player/PlayerTeleport.cs
+++ Assets/Main/Scripts/Player/PlayerTeleport.cs
@@ -6,20 +6,24 @@ public class PlayerTeleport : MonoBehaviour
 {
     [SerializeField] GameObject teleportEffect;
     Vector3 targetPos;
-    ParticleSystem[] pss;
+    ParticleSystem[] pss;    
     // Start is called before the first frame update
     void Start()
     {
         if (teleportEffect == null) teleportEffect = transform.Find("TeleportEffect").gameObject;
         pss = teleportEffect.GetComponentsInChildren<ParticleSystem>();
 
-        targetPos = Vector3.zero;
+        targetPos = Vector3.zero;        
     }
 
     public void TeleportTo(Vector3 pos) {
         if (targetPos == Vector3.zero && pos != Vector3.zero)
         {
             targetPos = pos;
+            GetComponent<CharacterController>().enabled = false;
+            GetComponent<PlayerInput>().enabled = false;
+            GetComponent<PlayerAiming>().enabled = false;
+            GetComponent<PlayerLocomotion>().enabled = false;
             PlayEffect();
             Invoke("Teleport", 2.5f);
         }        
@@ -27,12 +31,13 @@ public class PlayerTeleport : MonoBehaviour
     }
 
     void Teleport() {
-        if (targetPos != Vector3.zero) {
-            //StopEffect();
-            GetComponent<CharacterController>().enabled = false;
+        if (targetPos != Vector3.zero) {                        
             transform.position = targetPos;
             GetComponent<CharacterController>().enabled = true;
-            //PlayEffect();
+            GetComponent<PlayerInput>().enabled = true;
+            GetComponent<PlayerAiming>().enabled = true;
+            GetComponent<PlayerLocomotion>().enabled = true;
+            targetPos = Vector3.zero;           
         }
     }
 
