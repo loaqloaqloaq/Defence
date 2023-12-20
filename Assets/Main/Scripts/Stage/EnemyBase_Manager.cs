f570eeff0480e75e9e5fcc5fcb3ae6d11639dbd2
Cheung Ho Nam <63897284+loaqloaqloaq@users.noreply.github.com>
2023-12-20 13:44:50 +0900

    map

--- Assets/Main/Scripts/Stage/EnemyBase_Manager.cs
+++ Assets/Main/Scripts/Stage/EnemyBase_Manager.cs
@@ -15,14 +15,15 @@ public class EnemyBase_Manager : MonoBehaviour
     private float teleportCounter;
     public bool teleportFlg;
 
-    public GameObject[] enemyBase = new GameObject[3];　//現在マップに生成されているEnemyBase
-    private Vector3[] movePoint = new Vector3[9];//EnemyBaseの移動先
+    public GameObject[] enemyBase = new GameObject[2];　//現在マップに生成されているEnemyBase
+    private Vector3[] movePoint = new Vector3[6];//EnemyBaseの移動先
     public GameObject EnemyBase_Prefab;//EnemyBaseが壊されたら再生成するためのプレファブ
     public GameObject[] PlayerMovePoint = new GameObject[3];//一時的に強制移動するため、プレイヤーの移動先
     private GameObject Player;
 
     public int[] stage = new int[2] { 0, 0 };//壊れてるゲートの確認
-    public bool[] moveFlg = new bool[2] {false,false}; //移動したかの確認
+    public bool[] moveFlg = new bool[2] {false,false}; //ワープを一度実行するフラグ
+    private bool[] BaseMove = new bool[2] { false, false }; //そのステージに一度ワープしているかの確認
     // Start is called before the first frame update
     void Start()
     {
@@ -40,7 +41,7 @@ public class EnemyBase_Manager : MonoBehaviour
     void Update()
     {
         Move();
-        if (enemyBase[0] == null && enemyBase[1] == null && enemyBase[2] == null)
+        if (enemyBase[0] == null && enemyBase[1] == null)
         {
             Retreat();
         }
@@ -90,75 +91,62 @@ public class EnemyBase_Manager : MonoBehaviour
     {
         if (stage[0] != 0 && moveFlg[0])
         {
-            if (enemyBase[0] != null) enemyBase[0].gameObject.transform.position = movePoint[3];
+            if (enemyBase[0] != null) enemyBase[0].gameObject.transform.position = movePoint[2];
+            else
+            {
+                GameObject obj = Instantiate(EnemyBase_Prefab, movePoint[2], Quaternion.Euler(0, 180, 0));
+                enemyBase[0] = obj;
+            }
+            if (enemyBase[1] != null) enemyBase[1].gameObject.transform.position = movePoint[3];
             else
             {
                 GameObject obj = Instantiate(EnemyBase_Prefab, movePoint[3], Quaternion.Euler(0, 180, 0));
-                enemyBase[0] = obj;
-            }
-            if (enemyBase[1] != null) enemyBase[1].gameObject.transform.position = movePoint[4];
-            else
-            {
-                GameObject obj = Instantiate(EnemyBase_Prefab, movePoint[4], Quaternion.Euler(0, 180, 0));
                 enemyBase[1] = obj;
             }
-            if (enemyBase[2] != null) enemyBase[2].gameObject.transform.position = movePoint[5];
-            else
-            {
-                GameObject obj = Instantiate(EnemyBase_Prefab, movePoint[5], Quaternion.Euler(0, 180, 0));
-                enemyBase[2] = obj;
-            }
+            moveFlg[0] = false;
+            BaseMove[0] = true;
         }
         if (stage[1] != 0 && moveFlg[1])
         {
-            if (enemyBase[0] != null) enemyBase[0].gameObject.transform.position = movePoint[6];
+            if (enemyBase[0] != null) enemyBase[0].gameObject.transform.position = movePoint[4];
             else
             {
-                GameObject obj = Instantiate(EnemyBase_Prefab, movePoint[6], Quaternion.Euler(0, 180, 0));
+                GameObject obj = Instantiate(EnemyBase_Prefab, movePoint[4], Quaternion.Euler(0, 180, 0));
                 enemyBase[0] = obj;
             }
-            if (enemyBase[1] != null) enemyBase[1].gameObject.transform.position = movePoint[7];
+            if (enemyBase[1] != null) enemyBase[1].gameObject.transform.position = movePoint[5];
             else
             {
-                GameObject obj = Instantiate(EnemyBase_Prefab, movePoint[7], Quaternion.Euler(0, 180, 0));
+                GameObject obj = Instantiate(EnemyBase_Prefab, movePoint[5], Quaternion.Euler(0, 180, 0));
                 enemyBase[1] = obj;
             }
-            if (enemyBase[2] != null) enemyBase[2].gameObject.transform.position = movePoint[8];
-            else
-            {
-                GameObject obj = Instantiate(EnemyBase_Prefab, movePoint[8], Quaternion.Euler(0, 180, 0));
-                enemyBase[2] = obj;
-            }
+            moveFlg[1] = false;
+            BaseMove[1] = true;
         }
-
     }
 
     //ゲートを壊し前進したが全ての拠点が破壊されてしまった場合
     private void Retreat()
     {
         //第3ステージで破壊された場合
-        if (stage[1] != 0 && moveFlg[1])
+        if (stage[1] != 0 && BaseMove[1])
         {
-            GameObject obj = Instantiate(EnemyBase_Prefab, movePoint[3], Quaternion.Euler(0, 180, 0));
+            GameObject obj = Instantiate(EnemyBase_Prefab, movePoint[2], Quaternion.Euler(0, 180, 0));
             enemyBase[0] = obj;
-            obj = Instantiate(EnemyBase_Prefab, movePoint[4], Quaternion.Euler(0, 180, 0));
+            obj = Instantiate(EnemyBase_Prefab, movePoint[3], Quaternion.Euler(0, 180, 0));
             enemyBase[1] = obj;
-            obj = Instantiate(EnemyBase_Prefab, movePoint[5], Quaternion.Euler(0, 180, 0));
-            enemyBase[2] = obj;
             stage[1] = 0;
-            moveFlg[1] = false;
+            BaseMove[1] = false;
         }
         //第2ステージですべて破壊された場合
-        else if (stage[0] != 0 && moveFlg[0])
+        else if (stage[0] != 0 && BaseMove[0])
         {
             GameObject obj = Instantiate(EnemyBase_Prefab, movePoint[0], Quaternion.Euler(0, 180, 0));
             enemyBase[0] = obj;
             obj = Instantiate(EnemyBase_Prefab, movePoint[1], Quaternion.Euler(0, 180, 0));
             enemyBase[1] = obj;
-            obj = Instantiate(EnemyBase_Prefab, movePoint[2], Quaternion.Euler(0, 180, 0));
-            enemyBase[2] = obj;
             stage[0] = 0;
-            moveFlg[0] = false;
+            BaseMove[0] = false;
         }
         else
         {
