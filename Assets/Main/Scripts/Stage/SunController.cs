d2248a0e73266b9687d20284045908f70724dca8
Cheung Ho Nam <63897284+loaqloaqloaq@users.noreply.github.com>
2024-01-09 14:34:50 +0900

    ショップ修正

--- Assets/Main/Scripts/Stage/SunController.cs
+++ Assets/Main/Scripts/Stage/SunController.cs
@@ -5,16 +5,49 @@ using UnityEngine;
 
 public class SunController : MonoBehaviour
 {
-    [SerializeField]float rotateSpeed;
+    private Material skybox;     
+    private float Tfactor;
+
+    [SerializeField] Material nightSkyBox;
+    [SerializeField] float rotateSpeed;
+    [SerializeField] bool isNight;
+    [SerializeField] float NightValue, DayValue;
+    [SerializeField] float DayNigntChangeSpeed;
     // Start is called before the first frame update
     void Start()
     {
         if (rotateSpeed <= 0) rotateSpeed = 1f;
+        skybox = RenderSettings.skybox;        
+        isNight = false;
+        if (NightValue <= 0) NightValue = 0.1f;
+        if (DayValue <= 0) DayValue = 0.5f;
+        if (DayNigntChangeSpeed <= 0) DayNigntChangeSpeed = 0.5f;
+
+        Tfactor = DayValue;
+        skybox.SetColor("_Tint", new Color(Tfactor, Tfactor, Tfactor, 1));
     }
 
     // Update is called once per frame
     void Update()
     {               
-       transform.Rotate(-rotateSpeed*Time.deltaTime, 0, 0, Space.Self);       
+        transform.Rotate(-rotateSpeed*Time.deltaTime, 0, 0, Space.Self);
+        Debug.Log(transform.localRotation.eulerAngles);
+
+        isNight = transform.localRotation.eulerAngles.x > 200f;
+
+        if (isNight && Tfactor >= NightValue)
+        {
+            Tfactor -= Time.deltaTime * DayNigntChangeSpeed;
+            skybox.SetColor("_Tint", new Color(Tfactor, Tfactor, Tfactor, 1));
+            if (Tfactor <= NightValue) { 
+            
+            }
+        }
+        else if(!isNight && Tfactor <= DayValue) {
+            Tfactor += Time.deltaTime * DayNigntChangeSpeed;
+            skybox.SetColor("_Tint", new Color(Tfactor, Tfactor, Tfactor, 1));
+        }
+
+
     }
 }
