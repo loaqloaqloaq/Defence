4e0d042edf32cd99741693ff8632a048fbcbb31a
loaqloaqloaq <63897284+loaqloaqloaq@users.noreply.github.com>
2024-01-10 19:56:32 +0900

    player light point at crosshair

--- Assets/Scripts/Player/PlayerLight.cs
+++ Assets/Scripts/Player/PlayerLight.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class PlayerLight : MonoBehaviour
 {
-    [SerializeField] GameObject light;
+    [SerializeField] GameObject playerLight;
     [SerializeField] Transform CameraLookAt;
     bool lightOn;
     // Start is called before the first frame update
     void Start()
     {
-        if(light == null)light = transform.Find("light").gameObject;
+        if(playerLight == null) playerLight = transform.Find("light").gameObject;
         lightOn = false;
-        light.SetActive(lightOn);
+        playerLight.SetActive(lightOn);
     }
 
     // Update is called once per frame
@@ -21,10 +22,22 @@ public class PlayerLight : MonoBehaviour
         if (Input.GetButtonDown("Player Light"))
         {
             lightOn = !lightOn;
-            light.SetActive(lightOn);
+            playerLight.SetActive(lightOn);
         }
-        if (lightOn && CameraLookAt) { 
-            light.transform.localEulerAngles= CameraLookAt.localEulerAngles;
+        if (lightOn && CameraLookAt) {
+            RaycastHit hitInfo;
+            Ray ray=new Ray();
+            ray.origin = Camera.main.transform.position;
+            ray.direction = Camera.main.transform.forward;
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                playerLight.transform.LookAt(hitInfo.point);
+            }
+            else
+            {
+                playerLight.transform.localEulerAngles = CameraLookAt.localEulerAngles;
+            }
+            
         }
     }
 }
