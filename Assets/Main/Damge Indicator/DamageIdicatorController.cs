395248e4d65f5d6085d198a07034b7a9f86e00b6
loaqloaqloaq <63897284+loaqloaqloaq@users.noreply.github.com>
2024-01-23 21:11:51 +0900

    攻撃者の方向表示

---
+++ Assets/Main/Damge Indicator/DamageIdicatorController.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DamageIdicatorController : MonoBehaviour
+{
+    Image image;
+    Color originalColor,c;
+    float showTime;
+    float showTimer;
+
+    GameObject player,key;
+    Vector3 targetPos;
+
+    RectTransform rectTransform;
+
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(!image) image=transform.GetComponentInChildren<Image>();
+        originalColor=image.color;
+        player = GameObject.FindGameObjectWithTag("Player");
+        rectTransform = transform.GetComponent<RectTransform>();
+        targetPos = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //表示位置更新
+        updateRotation();
+        //表示時間過ぎた後段々薄くなる
+        showTimer += Time.deltaTime;
+        if (showTimer >= showTime)
+        {
+            if (c.a > 0)
+            {
+                c.a -= originalColor.a * Time.deltaTime;
+                image.color = c;
+            }
+            else remove();
+
+        }        
+    }
+
+    public void updatePos(GameObject key) {
+        c = originalColor;
+        image.color = c;
+        showTime = 1;
+        showTimer = 0;
+        targetPos = key.transform.position;
+        rectTransform.anchoredPosition = Vector2.zero;
+        this.key = key;
+    }
+
+    private void updateRotation() {
+        Vector3 targetDir = targetPos - player.transform.position;
+        float angle = Vector3.Angle(targetDir, player.transform.forward);
+        Debug.Log(angle);
+        var an=rectTransform.eulerAngles;
+        an.z = angle;
+        rectTransform.eulerAngles=an;
+    }
+
+    private void remove() {
+        DamageIndicator.Remove(key);
+    }
+    
+}
---
+++ Assets/Main/Damge Indicator/DamageIndicator.cs
@@ -0,0 +1,50 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageIndicator : MonoBehaviour
+{
+    [SerializeField] GameObject indicatorPrefab;
+    Dictionary<GameObject, GameObject> indicators=new Dictionary<GameObject, GameObject>();
+
+    static DamageIndicator i;
+    public static DamageIndicator Instance
+    {
+        get { 
+            if(i==null) i=FindObjectOfType<DamageIndicator>();
+            return i;
+        }
+    }
+    public static void Set(GameObject attacker) {
+        Instance._set(attacker);
+    }
+    public static void Remove(GameObject key) {
+        Instance.indicators[key].SetActive(false);
+        Instance.indicators.Remove(key);
+    }
+
+    void _set(GameObject attacker) {
+        if (attacker == null || attacker.activeSelf == false) return;
+        Debug.Log(attacker);
+        if (!indicators.ContainsKey(attacker)) {
+            GameObject indicator = null;
+            foreach (Transform child in transform) {
+                if (child.gameObject.activeSelf == false) {
+                    indicator=child.gameObject; 
+                    indicator.SetActive(true);
+                    break;
+                }
+            }
+            if (indicator == null)
+            {
+                indicator = Instantiate(indicatorPrefab);
+                indicator.transform.parent = transform;
+            }
+            indicators.Add(attacker, indicator);
+        }
+
+        if (indicators.ContainsKey(attacker)) indicators[attacker].GetComponent<DamageIdicatorController>().updatePos(attacker);
+        else Debug.LogWarning("indicator not found");
+    }   
+}
