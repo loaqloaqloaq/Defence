a739eb1341271980571b02c89c8b46f399eb412e
Cheung Ho Nam <63897284+loaqloaqloaq@users.noreply.github.com>
2024-01-24 14:11:45 +0900

    minor bug fix

--- Assets/Main/Scripts/Enemy/ExplosionController.cs
+++ Assets/Main/Scripts/Enemy/ExplosionController.cs
@@ -6,16 +6,19 @@ public class ExplosionController : MonoBehaviour
 {
     ParticleSystem ps;
     float timePassed;
+    AudioSource audio;
     // Start is called before the first frame update
     void Start()
     {
         ps=GetComponent<ParticleSystem>();
         timePassed = 0;
+        audio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        audio.volume = transform.localScale.x/3;
         timePassed += Time.deltaTime;
         if(timePassed >= ps.main.duration) Destroy(gameObject);
     }
